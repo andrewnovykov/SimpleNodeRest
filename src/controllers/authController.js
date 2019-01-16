c2ede684934ab91dd84462d2609e22a904c4101d
papapin <papapin777#gmail.com>
2019-01-15 20:22:39 -0800

    add registration

---
+++ src/controllers/authController.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/register', async (req, res)=> {
+
+    const { email } = req.body;
+
+    try {
+
+        if (await User.findOne({email})) {
+            return res
+              .status(400)
+              .send({ error: "user already exist!" });
+        }
+
+        const user = await User.create(req.body);
+        user.password = undefined;
+        return res.send({user});
+
+    } catch (err) {
+
+        return res.status(400).send({ error: 'registration failed!' });
+
+    }
+})
+
+module.exports = app => app.use('/auth', router);
