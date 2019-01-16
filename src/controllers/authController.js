9c130aa04c21796245b57cf707a7bac0870c494c
papapin <papapin777#gmail.com>
2019-01-15 21:07:20 -0800

    add authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,18 @@
 const express = require("express");
-
 const User = require('../models/user');
-
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
+
+
+function generateToken(params = {}){
+    return jwt.sign( params, authConfig.secret, {
+        expiresIn: 86400
+    })
+}
+
+
 
 router.post('/register', async (req, res)=> {
 
@@ -18,7 +28,10 @@ router.post('/register', async (req, res)=> {
 
         const user = await User.create(req.body);
         user.password = undefined;
-        return res.send({user});
+        return res.send({
+            user,
+            token: generateToken({ id: user.id })
+        });
 
     } catch (err) {
 
@@ -26,5 +39,32 @@ router.post('/register', async (req, res)=> {
 
     }
 })
+
+
+
+
+router.post("/authentication", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) return res.status(400).send({ error: "User not found!" });
+
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(400).send({ error: "Invalid password" });
+
+    user.password = undefined;
+
+    
+  res.send({ 
+      user,
+      token: generateToken({id: user.id})
+     });
+});
+
+
+
+
+
 
 module.exports = app => app.use('/auth', router);
