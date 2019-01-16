04b98d83d363710375a4620198946972943a8ad4
papapin <papapin777#gmail.com>
2019-01-15 21:18:02 -0800

    add projects ctrl

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 require('./controllers/authController')(app);
+require("./controllers/projectController")(app);
 
 app.listen(3000, ()=>{
     console.log('Server was started ....');
